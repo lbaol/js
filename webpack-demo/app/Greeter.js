9f5ef402389709f70df31e5ef1c4803ea4ce47da
BAO LEI <lei.baol@alibaba-inc.com>
2016-12-01 17:06:38 +0800

    update

---
+++ webpack-demo/app/Greeter.js
@@ -0,0 +1,16 @@
+//Greeter,js
+import React, {Component} from 'react'
+
+import styles from './Greeter.css';//导入
+
+class Greeter extends Component{
+  render() {
+    return (
+      <div>
+        Hello Greeter !!!!
+      </div>
+    );
+  }
+}
+
+export default Greeter
